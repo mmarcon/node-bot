2f21613876d8c12d68a5406d5537f692b3872826
Massimiliano Marcon <massimiliano.marcon@here.com>
2014-04-17 09:44:34 +0200

    try catch around requiring modules

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-var config = require('./config'),
-    Core = require('./lib/core'),
-    Module = require('./lib/modules/module'),
-    Protocol = require('./lib/protocols/Protocol'),
-    logger = require('./lib/logger'),
-    util = require('util');
+try {
+    var config = require('./config'),
+        Core = require('./lib/core'),
+        Module = require('./lib/modules/module'),
+        Protocol = require('./lib/protocols/Protocol'),
+        logger = require('./lib/logger'),
+        util = require('util');
 
-var ProtocolConstructor = require(config.protocol)(Protocol, util.inherits);
+    var ProtocolConstructor = require(config.protocol)(Protocol, util.inherits);
 
-var protocol = new ProtocolConstructor();
+    var protocol = new ProtocolConstructor();
 
-var loadedModules = config.modules.map(function(modulePath){
-    var ModuleConstructor = require(modulePath)(Module, util.inherits, logger);
-    return new ModuleConstructor();
-});
+    var loadedModules = config.modules.map(function(modulePath){
+        var ModuleConstructor = require(modulePath)(Module, util.inherits, logger);
+        return new ModuleConstructor();
+    });
 
-var core = new Core(
-    protocol,
-    loadedModules);
+    var core = new Core(
+        protocol,
+        loadedModules);
 
-process.on('SIGINT', function() {
-    core.stop();
-    process.exit(0);
-});
+    process.on('SIGINT', function() {
+        core.stop();
+        process.exit(0);
+    });
 
-core.start();
+    core.start();
+} catch(e) {
+    console.error('Something wrong here. Did you add config.json?')
+}
